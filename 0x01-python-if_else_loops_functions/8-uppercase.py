45e7672355222205f7af9c21c1c6149bd6e6cd14
Omokhodion Usifo <omokhodionusifo@gmail.com>
2023-03-07 18:57:22 -0800

    tasks

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def uppercase(str):
-    for ch in str:
-        if ord(ch) >= 97 and ord(ch) <= 122:
-            ch = chr(ord(ch) - 32)
-        print("{:s}".format(ch), end='')
+    for c in str:
+        if c >= ord('a') and c <= ord('2'):
+            c = chr(ord(c) - 32)
+            print("{}".format(c))
 
-    print('\n', end="")
+uppercase("abc")
