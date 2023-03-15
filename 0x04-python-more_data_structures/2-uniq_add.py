ba00d98b537ec9da63f50aa242f3cdd15b720ba9
Omokhodion Usifo <omokhodionusifo@gmail.com>
2023-03-15 11:53:37 -0700

    mandatory tasks

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-
 def uniq_add(my_list=[]):
-        new_list = []
-        for i in my_list:
-            if i not in new_list:
-                new_list.append(i)
-        return sum(new_list)
+    add = 0
+    for i in set(my_list):
+        add += i
+    return add
